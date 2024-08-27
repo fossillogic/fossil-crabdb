408a3362077679bd17c90e0c31e1e32725adcc21
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2024-08-26 19:59:17 -0600

    resolve issues from test

--- code/tests/test_database.c
+++ code/tests/test_database.c
@@ -92,7 +92,7 @@ FOSSIL_TEST(test_delete_value) {
 FOSSIL_TEST(test_crabql_create_namespace) {
     ASSUME_NOT_CNULL(db);
 
-    const char *query = "create_namespace('namespace1')";
+    const char *query = "create_namespace('namespace1');";
     fossil_crabdb_error_t result = fossil_crabdb_execute_query(db, query);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 }
@@ -101,12 +101,12 @@ FOSSIL_TEST(test_crabql_create_sub_namespace) {
     ASSUME_NOT_CNULL(db);
 
     // Create the parent namespace
-    const char *create_parent = "create_namespace('namespace1')";
+    const char *create_parent = "create_namespace('namespace1');";
     fossil_crabdb_error_t result = fossil_crabdb_execute_query(db, create_parent);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 
     // Create the sub-namespace
-    const char *create_sub = "create_sub_namespace('namespace1', 'subnamespace1')";
+    const char *create_sub = "create_sub_namespace('namespace1', 'subnamespace1');";
     result = fossil_crabdb_execute_query(db, create_sub);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 }
@@ -115,12 +115,12 @@ FOSSIL_TEST(test_crabql_erase_namespace) {
     ASSUME_NOT_CNULL(db);
 
     // Create a namespace
-    const char *create_ns = "create_namespace('namespace1')";
+    const char *create_ns = "create_namespace('namespace1');";
     fossil_crabdb_error_t result = fossil_crabdb_execute_query(db, create_ns);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 
     // Erase the namespace
-    const char *erase_ns = "erase_namespace('namespace1')";
+    const char *erase_ns = "erase_namespace('namespace1');";
     result = fossil_crabdb_execute_query(db, erase_ns);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 
@@ -140,14 +140,14 @@ FOSSIL_TEST(test_crabql_erase_sub_namespace) {
     ASSUME_NOT_CNULL(db);
 
     // Create the parent namespace and a sub-namespace
-    const char *create_parent = "create_namespace('namespace1')";
-    const char *create_sub = "create_sub_namespace('namespace1', 'subnamespace1')";
+    const char *create_parent = "create_namespace('namespace1');";
+    const char *create_sub = "create_sub_namespace('namespace1', 'subnamespace1');";
     fossil_crabdb_error_t result = fossil_crabdb_execute_query(db, create_parent);
     result = fossil_crabdb_execute_query(db, create_sub);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 
     // Erase the sub-namespace
-    const char *erase_sub = "erase_sub_namespace('namespace1', 'subnamespace1')";
+    const char *erase_sub = "erase_sub_namespace('namespace1', 'subnamespace1');";
     result = fossil_crabdb_execute_query(db, erase_sub);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 
@@ -200,12 +200,12 @@ FOSSIL_TEST(test_crabql_rename_namespace) {
     ASSUME_NOT_CNULL(db);
 
     // Create a namespace
-    const char *create_ns = "create_namespace('namespace1')";
+    const char *create_ns = "create_namespace('namespace1');";
     fossil_crabdb_error_t result = fossil_crabdb_execute_query(db, create_ns);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 
     // Rename the namespace
-    const char *rename_ns = "rename_namespace('namespace1', 'namespace2')";
+    const char *rename_ns = "rename_namespace('namespace1', 'namespace2');";
     result = fossil_crabdb_execute_query(db, rename_ns);
     ASSUME_ITS_EQUAL_I32(CRABDB_OK, result);
 
