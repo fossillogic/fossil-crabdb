445864fdd395d806243ac8daf19517fd6394c8ba
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2024-08-30 15:10:12 -0600

    remove two cases

--- code/tests/test_query.c
+++ code/tests/test_query.c
@@ -47,22 +47,6 @@ FOSSIL_TEST(test_fossil_crabdb_load) {
     free(loaded_db);
 }
 
-FOSSIL_TEST(test_fossil_crabdb_execute_query_create_namespace) {
-    FILE *file = fopen("create_namespace_query.crabql", "w");
-    fprintf(file, "namespace test_namespace\n");
-    fclose(file);
-
-    fossil_crabdb_t *db = fossil_crabdb_create();
-    int result = fossil_crabdb_execute_query(db, "create_namespace_query.crabql");
-    ASSUME_ITS_EQUAL_I32(result, CRABDB_OK);
-
-    fossil_crabdb_namespace_t *ns = fossil_crabdb_find_namespace(db, "test_namespace");
-    ASSUME_NOT_CNULL(ns);
-    ASSUME_ITS_EQUAL_CSTR(ns->name, "test_namespace");
-
-    free(db);
-}
-
 FOSSIL_TEST(test_fossil_crabdb_execute_query_delete_namespace) {
     FILE *file = fopen("delete_namespace_query.crabql", "w");
     fprintf(file, "namespace test_namespace\n");
@@ -73,24 +57,6 @@ FOSSIL_TEST(test_fossil_crabdb_execute_query_delete_namespace) {
     fossil_crabdb_execute_query(db, "delete_namespace_query.crabql");
     fossil_crabdb_namespace_t *ns = fossil_crabdb_find_namespace(db, "test_namespace");
     ASSUME_ITS_CNULL(ns);
-
-    free(db);
-}
-
-FOSSIL_TEST(test_fossil_crabdb_execute_query_set_key_value) {
-    FILE *file = fopen("set_key_value_query.crabql", "w");
-    fprintf(file, "namespace test_namespace\n");
-    fprintf(file, "set key=key1 value='value1'\n");
-    fclose(file);
-
-    fossil_crabdb_t *db = fossil_crabdb_create();
-    fossil_crabdb_execute_query(db, "set_key_value_query.crabql");
-
-    fossil_crabdb_namespace_t *ns = fossil_crabdb_find_namespace(db, "test_namespace");
-    ASSUME_NOT_CNULL(ns);
-    const char *value = fossil_crabdb_get_value(ns, "key1");
-    ASSUME_NOT_CNULL(value);
-    ASSUME_ITS_EQUAL_CSTR(value, "value1");
 
     free(db);
 }
@@ -126,9 +92,7 @@ FOSSIL_TEST(test_fossil_crabdb_execute_query_invalid_command) {
 FOSSIL_TEST_GROUP(c_crabql_query_tests) {
     ADD_TEST(test_fossil_crabdb_save);
     ADD_TEST(test_fossil_crabdb_load);
-    // ADD_TEST(test_fossil_crabdb_execute_query_create_namespace);
     ADD_TEST(test_fossil_crabdb_execute_query_delete_namespace);
-    // ADD_TEST(test_fossil_crabdb_execute_query_set_key_value);
     ADD_TEST(test_fossil_crabdb_execute_query_non_existent_namespace);
     ADD_TEST(test_fossil_crabdb_execute_query_invalid_command);
 }
